966421b1b78753be24a5a7dd147ca776c68dc78d
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2021-02-14 22:55:31 +0600

    final

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
+const searchTitle = document.getElementById("id1");
+const searchArea =  document.getElementById("id2");
+const createAnotherArea = document.getElementById("id3");
+const createAnotherInput = document.getElementById("id4");
 // selected image 
 let sliders = [];
 
@@ -22,7 +26,9 @@ const showImages = (images) => {
   images.forEach(image => {
     let div = document.createElement('div');
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
+    let str = `${image.tags}`;
+    str =  str.replaceAll(' ','#');
+    div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}","${str}","${image.id}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
   })
 
@@ -31,24 +37,41 @@ const showImages = (images) => {
 const getImages = (query) => {
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hitS))
+    .then(data => showImages(data.hits))
     .catch(err => console.log(err))
 }
 
 let slideIndex = 0;
-const selectItem = (event, img) => {
+const selectItem = (event, img , tags, id) => {
   let element = event.target;
   element.classList.add('added');
- 
-  let item = sliders.indexOf(img);
+  let obj = {
+    img:img,
+    tags: tags,
+    Id:id
+  }
+  let item = -1,cnt = 0;
+  sliders.forEach(i => {
+     if(id === i.Id) {
+       item = cnt;
+     }
+     cnt++;
+  });
   if (item === -1) {
-    sliders.push(img);
+    sliders.push(obj);
   } else {
-    alert('Hey, Already added !')
+    sliders.splice(item, 1);
+    element.classList.remove('added');
+    //alert('Hey, Already added !')
   }
 }
 var timer
 const createSlider = () => {
+  const duration = document.getElementById('duration').value || 1000;
+  if(duration<0) {
+    alert("Please input positive slide Change duration");
+    return;
+  }
   // check slider image length
   if (sliders.length < 2) {
     alert('Select at least 2 image.')
@@ -67,12 +90,20 @@ const createSlider = () => {
   document.querySelector('.main').style.display = 'block';
   // hide image aria
   imagesArea.style.display = 'none';
-  const duration = document.getElementById('duration').value || 1000;
+  searchTitle.style.display = 'none';
+  searchArea.style.display = 'none';
+  searchArea.classList.remove("d-flex");
+
+  createAnotherArea.style.display = 'block';
+  createAnotherArea.classList.add("d-flex");
+  let cnt=0;
+  
   sliders.forEach(slide => {
+    let str = slide.tags.replaceAll('#',' ');
     let item = document.createElement('div')
     item.className = "slider-item";
-    item.innerHTML = `<img class="w-100"
-    src="${slide}"
+    item.innerHTML = `<div class="tags-txt">${str}</div><img class="w-100"
+    src="${slide.img}"
     alt="">`;
     sliderContainer.appendChild(item)
   })
@@ -109,6 +140,17 @@ const changeSlide = (index) => {
   items[index].style.display = "block"
 }
 
+function createNewSlider(){
+  document.querySelector('.main').style.display = 'none';
+  clearInterval(timer);
+  createAnotherArea.classList.remove("d-flex");
+  createAnotherArea.style.display='none';
+  searchArea.classList.add("d-flex");
+  searchArea.style.display = 'block';
+  searchTitle.style.display = 'block';
+
+}
+
 searchBtn.addEventListener('click', function () {
   document.querySelector('.main').style.display = 'none';
   clearInterval(timer);
@@ -116,6 +158,19 @@ searchBtn.addEventListener('click', function () {
   getImages(search.value)
   sliders.length = 0;
 })
+
+document.getElementById('search').addEventListener('keypress', function(event) {
+  if (event.keyCode == 13) {
+    document.querySelector('.main').style.display = 'none';
+    clearInterval(timer);
+    const search = document.getElementById('search');
+    getImages(search.value)
+    sliders.length = 0;
+  }
+});
+
+createAnotherArea.style.display = 'none';
+createAnotherArea.classList.remove("d-flex");
 
 sliderBtn.addEventListener('click', function () {
   createSlider()
